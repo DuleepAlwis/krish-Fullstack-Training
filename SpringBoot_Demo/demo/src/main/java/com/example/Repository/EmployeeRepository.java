f583fadbb40688b7d6ecf376375d3c37a33eacc8
DuleepAlwis <duleepalwis@gmail.com>
2022-07-17 21:22:10 +0530

    Spring boot application changes

--- SpringBoot_Demo/demo/src/main/java/com/example/Repository/EmployeeRepository.java
+++ SpringBoot_Demo/demo/src/main/java/com/example/Repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.example.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.model.EmployeeEntity;
@@ -12,7 +13,11 @@ public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Integer
 
 	public EmployeeEntity save(EmployeeEntity entity);
 	
+	public EmployeeEntity getById(int id);
 	
+	 public List<EmployeeEntity> findAll();
 	
+	 @Query("select e from EmployeeEntity e where name=?1")
+	 public EmployeeEntity getByName(String name);
 	
 }
--- SpringBoot_Demo/demo/src/main/java/com/example/controllers/EmployeeController.java
+++ SpringBoot_Demo/demo/src/main/java/com/example/controllers/EmployeeController.java
@@ -2,9 +2,11 @@ package com.example.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Repository.EmployeeRepository;
@@ -23,6 +25,16 @@ public class EmployeeController {
 	public EmployeeEntity save(@RequestBody EmployeeEntity e){
 		
 		return empRepo.save(e);
+	}
+	
+	@RequestMapping(value="/getById/{id}", method=RequestMethod.GET)
+	public EmployeeEntity getById(@PathVariable Integer id) {
+		return empRepo.getById(id);
+	}
+	
+	@RequestMapping(value="/getByName",method=RequestMethod.GET)
+	public EmployeeEntity getByName(@RequestParam(value="name") String name ) {
+		return empRepo.getByName(name);
 	}
 	
 	@RequestMapping(value="/hello", method=RequestMethod.GET)
